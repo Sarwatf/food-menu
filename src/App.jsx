2364ccf854e30c5f2fd9d16da29cd849db1472f7
Sarwatf <sarwatfatima2006@gmail.com>
2024-08-03 13:48:49 +0500

    navbar-startup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <section>
+      <div className='Nav-bar'>
+        <img src="" alt="" />
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Menu</li>
+          </ul>
+
+          </div>
+    </section>
+  )
+}
+
+export default App
